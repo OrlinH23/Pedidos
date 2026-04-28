f291e5b729e744cb2ab366f0ba41ecbde0ba5434
Orlin Hernandez <orlinhernandez2389@gmail.com>
2026-04-27 19:23:44 -0600

    commit 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+
+Producto producto = new Producto();
+producto.Id=1;
+producto.Nombre="Mouse";
+producto.Precio= 250.00m;
+producto.Stock= 50;
+
+Console.WriteLine("Datos del producto:");
+
+Console.WriteLine($"Id: {producto.Id}");
+Console.WriteLine($"Nombre: {producto.Nombre}");
+Console.WriteLine($"Precio: {producto.Precio:C}");
+Console.WriteLine($"Cantidad Disponible: {producto.Stock}");
+Console.WriteLine();
+
+
+
+
+
+
